09fc4c7d808c4a50bfa70e48e07694c5478b6947
Nikolas Bogucarskis <niktechnopro@gmail.com>
2017-12-20 21:28:18 -0500

    first changes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import NavBar from './containers/NavBar'
+import NavBar from './containers/NavBar';
+import LoginPage from './containers/LoginPage';
 
 class App extends Component {
+	constructor(){
+		super()
+		this.handleLogin = this.handleLogin.bind(this);
+	}
+
+	handleLogin(event){
+		event.preventDefault();
+
+	}
+
   render() {
     return (
       <div className="App">
         <NavBar />
+        <LoginPage />
       </div>
     );
   }
